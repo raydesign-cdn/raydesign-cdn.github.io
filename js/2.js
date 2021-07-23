2fe513f40c7d496bf21c88c58719b5640a22e0a0
raydesign-cdn <ray_design@hotmail.com>
2021-07-23 13:49:09 +0800

    remove land park forms

--- js/2.js
+++
@@ -1,5 +0,0 @@
-// responsive table is no need for this page, so override func.js which create responsive table
-$(document).ready(function() {
-  $('table.hidden-lg').remove();
-  $('table.hidden-xs').removeClass('hidden-xs');
-});
